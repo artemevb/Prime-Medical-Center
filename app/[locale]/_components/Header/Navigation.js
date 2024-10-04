dc4d1d81ea39510b903870bb6179a32a8f8ec489
Andrey <aa71555443@gmail.com>
2024-10-04 11:02:02 +0500

    fixed header pl

--- app/[locale]/_components/Header/Navigation.js
+++ app/[locale]/_components/Header/Navigation.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navigation({ locale, navOptions }) {
   return (
-    <nav className="z-10 text-xl hidden lg:flex flex-grow justify-center transition-all duration-150">
+    <nav className="3xl:pl-[200px] z-10 text-xl hidden lg:flex flex-grow justify-center transition-all duration-150">
       <ul className="flex gap-1 justify-between self-stretch my-auto text-lg text-neutral-900 max-md:flex-wrap w-full max-w-[630px] transition-all duration-150">
         {navOptions.map((option) => (
           <Link href={`/${locale}${option.url}`} className="flex items-center gap-1 z-10 transition-all duration-300 hover:text-[#398f61] rounded-xl p-1 w-full text-center max-w-full justify-center font-semibold" key={option.id}>
